67f755e780ac32d9988d351037c0f864cc00b6a8
malol <malol@192.168.114.61>
2021-12-03 17:19:38 +0100

    Création du projet - Mise en page

--- Speed-Click/src/speed/click/SpeedClick.java
+++ Speed-Click/src/speed/click/SpeedClick.java
@@ -1,13 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+    Mini-Projet : Speed-Click
+    ROQUE Daphné & LAJOUS Malô
+    Décembre 2021
+
+*/
 package speed.click;
 
-/**
- *
- * @author malol
- */
 public class SpeedClick {
 
     /**
