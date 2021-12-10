08e4dbd0e371cba9e2cc82ffa098a2f30cdaf46a
malol <malol@192.168.114.61>
2021-12-10 11:49:01 +0100

    Classe Grille: Création de classe

---
+++ Speed-Click/src/speed/click/Grille.java
@@ -0,0 +1,15 @@
+/*
+    Classe Grille
+    Mini-Projet : Speed-Click
+    ROQUE Daphné & LAJOUS Malô
+    Décembre 2021
+*/
+package speed.click;
+
+/**
+ *
+ * @author malol
+ */
+public class Grille {
+    
+}
