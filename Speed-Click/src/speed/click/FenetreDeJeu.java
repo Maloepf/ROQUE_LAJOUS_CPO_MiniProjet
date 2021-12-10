d4a0e7ed9d476bddc8c3f5c084df8395649e4b5f
daphn <daphn@DESKTOP-6IM2TDU>
2021-12-10 15:32:03 +0100

    interface garphique : création des trois panels/renommage/caractéristiques -creation de la grille de boutons

--- Speed-Click/src/speed/click/FenetreDeJeu.java
+++ Speed-Click/src/speed/click/FenetreDeJeu.java
@@ -23,21 +23,144 @@ public class FenetreDeJeu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        grilleJeu = new javax.swing.JPanel();
+        cellule11 = new javax.swing.JButton();
+        cellule12 = new javax.swing.JButton();
+        cellule21 = new javax.swing.JButton();
+        cellule22 = new javax.swing.JButton();
+        chronometre = new javax.swing.JPanel();
+        chronometre1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        START = new javax.swing.JButton();
+        infosjoueur = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        scorejoueur = new javax.swing.JLabel();
+        nomjoueur = new javax.swing.JLabel();
+        recordnomjoueur = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        infospartie = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        mode1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 613, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 419, Short.MAX_VALUE)
-        );
+        grilleJeu.setBackground(new java.awt.Color(204, 255, 255));
+        grilleJeu.setPreferredSize(new java.awt.Dimension(520, 520));
+        grilleJeu.setLayout(new java.awt.GridLayout(2, 2));
 
-        pack();
+        cellule11.setBackground(new java.awt.Color(255, 255, 255));
+        cellule11.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cellule11ActionPerformed(evt);
+            }
+        });
+        grilleJeu.add(cellule11);
+        grilleJeu.add(cellule12);
+
+        cellule21.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cellule21ActionPerformed(evt);
+            }
+        });
+        grilleJeu.add(cellule21);
+        grilleJeu.add(cellule22);
+
+        getContentPane().add(grilleJeu, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 600, 600));
+
+        chronometre.setBackground(new java.awt.Color(255, 255, 204));
+        chronometre.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        chronometre1.setBackground(new java.awt.Color(255, 255, 204));
+        chronometre1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        chronometre.add(chronometre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 30, 310, 130));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setText("Chronomètre : ");
+        chronometre.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 100, 30));
+
+        START.setText("START");
+        START.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                STARTActionPerformed(evt);
+            }
+        });
+        chronometre.add(START, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, 140, 40));
+
+        getContentPane().add(chronometre, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 30, 310, 130));
+
+        infosjoueur.setBackground(new java.awt.Color(255, 255, 204));
+        infosjoueur.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel2.setText("Infos Joueurs : ");
+        infosjoueur.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 130, 20));
+
+        scorejoueur.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        scorejoueur.setText("scorejoueur");
+        infosjoueur.add(scorejoueur, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 90, 80, 20));
+
+        nomjoueur.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        nomjoueur.setText("nomjoueur");
+        infosjoueur.add(nomjoueur, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 50, 80, 20));
+
+        recordnomjoueur.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        recordnomjoueur.setText("reccordnomjoueur");
+        infosjoueur.add(recordnomjoueur, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 170, 120, 20));
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setText("Score : ");
+        infosjoueur.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 90, 80, 20));
+        infosjoueur.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 150, 270, -1));
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel6.setText("Nom : ");
+        infosjoueur.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, 80, 20));
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel7.setText("Reccord : ");
+        infosjoueur.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 170, 80, 20));
+
+        getContentPane().add(infosjoueur, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 180, 310, 250));
+
+        infospartie.setBackground(new java.awt.Color(255, 255, 204));
+        infospartie.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel3.setText("Selectionner mode de jeu : ");
+        infospartie.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 230, 20));
+
+        mode1.setText("mode1");
+        mode1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mode1ActionPerformed(evt);
+            }
+        });
+        infospartie.add(mode1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, -1, -1));
+
+        getContentPane().add(infospartie, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 460, 310, 120));
+
+        setBounds(0, 0, 996, 662);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void STARTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_STARTActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_STARTActionPerformed
+
+    private void mode1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mode1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mode1ActionPerformed
+
+    private void cellule11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cellule11ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cellule11ActionPerformed
+
+    private void cellule21ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cellule21ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cellule21ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -75,5 +198,26 @@ public class FenetreDeJeu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton START;
+    private javax.swing.JButton cellule11;
+    private javax.swing.JButton cellule12;
+    private javax.swing.JButton cellule21;
+    private javax.swing.JButton cellule22;
+    private javax.swing.JPanel chronometre;
+    private javax.swing.JPanel chronometre1;
+    private javax.swing.JPanel grilleJeu;
+    private javax.swing.JPanel infosjoueur;
+    private javax.swing.JPanel infospartie;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JButton mode1;
+    private javax.swing.JLabel nomjoueur;
+    private javax.swing.JLabel recordnomjoueur;
+    private javax.swing.JLabel scorejoueur;
     // End of variables declaration//GEN-END:variables
 }
